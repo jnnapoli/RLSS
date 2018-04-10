4f675b79e7c33b6446c03d1fd8024080290b60fa
jnnapoli <joey@jnnapoli.com>
2018-04-10 16:26:56 -0400

    added devise, scaffolded matches, worked on logic to link to VODs, cleaned up VOD table display

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+  before_filter :set_user_time_zone
+
+  private
+
+  def set_user_time_zone
+
+  end
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+
+
+  get 'users/sign_up' => redirect('404.html')
+
+
+  devise_for :users
+
   root 'welcome#index'
+
+  resources :matches
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
